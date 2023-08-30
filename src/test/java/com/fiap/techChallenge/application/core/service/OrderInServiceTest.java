25c329e9823fdddf35a80d0c5bb128757a9d3233
felipeksw <felipe.ksw@gmail.com>
2023-08-30 20:49:52 -0300

    feat: Adicionado tests unitarios para Status e OrderInService

--- src/test/java/com/fiap/techChallenge/application/core/service/OrderInServiceTest.java
+++ src/test/java/com/fiap/techChallenge/application/core/service/OrderInServiceTest.java
@@ -2,6 +2,8 @@ package com.fiap.techChallenge.application.core.service;
 
 import com.fiap.techChallenge.application.core.domain.Order;
 import com.fiap.techChallenge.application.core.domain.Status;
+import com.fiap.techChallenge.application.core.exceptions.InvalidDataException;
+import com.fiap.techChallenge.application.core.exceptions.NotFoundException;
 import com.fiap.techChallenge.application.ports.out.OrderOutPort;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,7 +14,10 @@ import org.mockito.MockitoAnnotations;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 public class OrderInServiceTest {
@@ -38,11 +43,71 @@ public class OrderInServiceTest {
         verify(orderOutPort, times(1)).save(ORDER);
     }
 
-    private Order buildOrder(){
+    @Test
+    public void givenInvalidStatus_WhenInsert_thenThrowInvalidData() {
+        Order invalidStatusOrder = buildOrder("recebido");
+
+        var receivedException = assertThrowsExactly(InvalidDataException.class,
+                () -> orderInService.insert(invalidStatusOrder),
+                "Exception different than expected!");
+
+        verify(orderOutPort, never()).save(any());
+        assertEquals("Invalid Status to insert order",
+                receivedException.getMessage(),
+                "Exception message different than expected!");
+    }
+
+    @Test
+    public void givenInvalidStatus_WhenUpdateStatus_thenThrowInvalidData() {
+        Order iniciedOrder = buildOrder("iniciado");
+        Long testId = 1l;
+        Status invalidStatus = new Status("recebido");
+
+        when(orderOutPort.findById(testId)).thenReturn(iniciedOrder);
+
+        var receivedException = assertThrowsExactly(InvalidDataException.class,
+                () -> orderInService.updateStatus(testId, invalidStatus),
+                "Exception different than expected!");
+
+        verify(orderOutPort, never()).update(any());
+        assertEquals("Status " + iniciedOrder.status().value() + " cannot be changed to " + invalidStatus.value(),
+                receivedException.getMessage(),
+                "Exception message different than expected!");
+    }
+
+    @Test
+    public void givenOrderIdNotRegistered_WhenUpdateStatus_thenThrowNotFound() {
+        Long testId = 1l;
+        Status status = new Status("recebido");
+
+        when(orderOutPort.findById(testId)).thenReturn(null);
+
+        var receivedException = assertThrowsExactly(NotFoundException.class,
+                () -> orderInService.updateStatus(testId, status),
+                "Exception different than expected!");
+
+        verify(orderOutPort, never()).update(any());
+        assertEquals("Ordem não encontrada",
+                receivedException.getMessage(),
+                "Exception message different than expected!");
+    }
+
+    private Order buildOrder() {
         return Order.builder()
                 .customerId(1L)
                 .price(new BigDecimal(20))
                 .status("iniciado")
+                .paymentMethod("CARTAO")
+                .client("Rodrigo")
+                .orderItems(new ArrayList<>())
+                .build();
+    }
+
+    private Order buildOrder(String status) {
+        return Order.builder()
+                .customerId(1L)
+                .price(new BigDecimal(20))
+                .status(status)
                 .paymentMethod("CARTAO")
                 .client("Rodrigo")
                 .orderItems(new ArrayList<>())
