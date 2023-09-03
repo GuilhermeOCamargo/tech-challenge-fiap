9ae415c616e9145f321c2eb291b86eb02d41b3a3
Felipe <felipe.ksw@gmail.com>
2023-09-02 23:03:41 -0300

    Ajuste para rodar em cluster k3s ou k8s

--- k8s/init.sh
+++ k8s/init.sh
@@ -1,9 +1,16 @@
-
-# kubectl apply -f volumes/database-pv.yaml
-# kubectl apply -f volumes/database-pvc.yaml
-# kubectl apply -f deployment/database-deployment.yaml
-
-
 kubectl create secret generic tech-challenge-db-user-secret --from-literal=MARIADB_USER=application_user
 kubectl create secret generic tech-challenge-db-password-secret --from-literal=MARIADB_PASSWORD=SENHA123
 kubectl create secret generic tech-challenge-db-root-password-secret --from-literal=MARIADB_ROOT_PASSWORD=tech-challenge-password
+
+kubectl apply -f ./volumes/storage-class.yaml
+kubectl apply -f ./volumes/database-pv.yaml 
+kubectl apply -f ./volumes/database-pvc.yaml 
+
+kubectl apply -f ./database/database-deployment.yaml
+kubectl apply -f ./database/database-svc.yaml
+
+sleep 90
+
+kubectl apply -f ./application/application-config-map.yaml
+kubectl apply -f ./application/application-deployment.yaml
+kubectl apply -f ./application/application-svc.yaml
