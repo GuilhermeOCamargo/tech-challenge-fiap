9707a336651c1ce16f3059b5f7ce0ae3d9adb667
Guilherme Camargo <guilherme.camargo@amedigital.com>
2023-09-04 19:37:43 -0300

    Ajustando conflitos de merge

--- k8s_deploy.sh
+++ k8s_deploy.sh
@@ -4,6 +4,5 @@ kubectl create secret generic tech-challenge-db-user-secret --from-literal=MARIA
 kubectl create secret generic tech-challenge-db-password-secret --from-literal=MARIADB_PASSWORD=SENHA123
 kubectl create secret generic tech-challenge-db-root-password-secret --from-literal=MARIADB_ROOT_PASSWORD=tech-challenge-password
 
-kubectl apply -f k8s/volumes 
 kubectl apply -f k8s/database 
 kubectl apply -f k8s/application 
