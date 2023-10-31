1d33fb2ad7dcf89225a599cebc6ecf4027f34ee5
Guilherme Camargo <guilherme.camargo@amedigital.com>
2023-10-31 15:25:00 -0300

    feat: ajustando cd para deploy no ECS

--- src/main/java/com/fiap/techChallenge/presentation/controllers/HealthController.java
+++ src/main/java/com/fiap/techChallenge/presentation/controllers/HealthController.java
@@ -1,5 +1,7 @@
 package com.fiap.techChallenge.presentation.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,13 @@ import java.util.Map;
 @RequestMapping("/health")
 public class HealthController {
 
+    private static final Logger LOG
+            = LoggerFactory.getLogger(HealthController.class);
+
     @GetMapping
     public Map<String, String> healthCheck() {
-        return Map.of("status", "OK");
+        var response = Map.of("status", "OK");
+        LOG.info("Endpoint de Health: ", response);
+        return response;
     }
 }
