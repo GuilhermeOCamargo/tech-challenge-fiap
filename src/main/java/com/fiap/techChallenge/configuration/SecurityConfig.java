b2ca8d89ef632c29d22236457e216437684e0520
Felipe <felipe.ksw@gmail.com>
2023-11-03 16:55:40 -0300

    Ajust resources secrity

--- src/main/java/com/fiap/techChallenge/configuration/SecurityConfig.java
+++ src/main/java/com/fiap/techChallenge/configuration/SecurityConfig.java
@@ -1,29 +1,57 @@
 package com.fiap.techChallenge.configuration;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class SecurityConfig {
 
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.cors(cors -> cors.disable());
+        return http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(antMatcher( "/health")).permitAll()
+                        .requestMatchers(antMatcher( "/swagger-ui/**")).permitAll()
+                        .requestMatchers(antMatcher( "/v3/api-docs/**")).permitAll()
+                        .requestMatchers(antMatcher( HttpMethod.PUT,"/order/**")).hasAnyRole("grp_admin")
+                        .requestMatchers(antMatcher( "/order/**")).permitAll()
+                        .requestMatchers(antMatcher( HttpMethod.POST,"/products/**")).hasAnyRole("grp_admin")
+                        .requestMatchers(antMatcher( HttpMethod.PUT,"/products/**")).hasAnyRole("grp_admin")
+                        .requestMatchers(antMatcher( HttpMethod.DELETE,"/products/**")).hasAnyRole("grp_admin")
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .requestMatchers(antMatcher("/customer/**")).permitAll()
+                        .requestMatchers(antMatcher("/**")).authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                )
+                .build();
+    }
 
-        http.csrf(csrf -> csrf.disable());
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("cognito:groups");
 
-        http.authorizeHttpRequests(expressionInterceptUrlRegistry -> expressionInterceptUrlRegistry
-                .anyRequest().denyAll())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
-
-        return http.build();
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
     }
 
 }
